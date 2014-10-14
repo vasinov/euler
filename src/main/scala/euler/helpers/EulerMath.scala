d6681c309d2ab64339b490a230bfde891b2fcd71
Vasily Vasinov <vasinov@me.com>
2014-10-13 22:16:58 -0600

    Added two more fibonacci generators to the EulerMath trait

--- src/main/scala/euler/helpers/EulerMath.scala
+++ src/main/scala/euler/helpers/EulerMath.scala
@@ -3,5 +3,12 @@ package euler.helpers
 import scala.math.BigInt
 
 trait EulerMath {
-  lazy val fibs: Stream[BigInt] = BigInt(0) #:: BigInt(1) #:: fibs.zip(fibs.tail).map { n => n._1 + n._2 }
+  val fibs: Stream[BigInt] = BigInt(0) #:: BigInt(1) #:: fibs.zip(fibs.tail).map { n => n._1 + n._2 }
+
+  val fibs2: Stream[BigInt] = {
+    def loop(h: BigInt, n: BigInt): Stream[BigInt] = h #:: loop(n, h + n)
+    loop(0, 1)
+  }
+
+  val fibs3: Stream[BigInt] = 0 #:: fibs3.scanLeft(BigInt(1))(_ + _)
 }
