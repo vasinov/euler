c9d938eef0066ce5a4afd497ea6df8fa8ee1a9f7
Vasily Vasinov <vasinov@me.com>
2014-10-18 20:15:22 -0600

    Don't need reverse

--- src/main/scala/euler/solutions/Solution004.scala
+++ src/main/scala/euler/solutions/Solution004.scala
@@ -3,8 +3,8 @@ package euler.solutions
 import euler.helpers.EulerStrings
 
 object Solution004 extends App with EulerStrings {
-  lazy val answer = (1 to 999).reverse
-    .flatMap(i => (i to 999).reverse.map(y => y * i))
+  lazy val answer = (1 to 999)
+    .flatMap(i => (i to 999).map(y => y * i))
     .filter(n => isPalindrome(n))
     .max
 
