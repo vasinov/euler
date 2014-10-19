c698709ce054ba81c9867c2b279ba387d5f7c7d4
Vasily Vasinov <vasinov@me.com>
2014-10-18 20:14:35 -0600

    Cleanup

--- src/main/scala/euler/solutions/Solution004.scala
+++ src/main/scala/euler/solutions/Solution004.scala
@@ -1,8 +1,8 @@
 package euler.solutions
 
-import euler.helpers.{EulerStrings, EulerMath}
+import euler.helpers.EulerStrings
 
-object Solution004 extends App with EulerMath with EulerStrings {
+object Solution004 extends App with EulerStrings {
   lazy val answer = (1 to 999).reverse
     .flatMap(i => (i to 999).reverse.map(y => y * i))
     .filter(n => isPalindrome(n))
